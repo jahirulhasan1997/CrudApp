c5bfddfc26111d7953e0ba1db7e20fa7bde3d00f
jahirulhasan1997 <52617147+jahirulhasan1997@users.noreply.github.com>
2021-01-14 17:36:07 +0530

    Update EmployeeController.java

--- src/main/java/com/example/springbootone/EmployeeController/EmployeeController.java
+++ src/main/java/com/example/springbootone/EmployeeController/EmployeeController.java
@@ -11,7 +11,7 @@ import com.example.springbootone.dao.UserRepository;
 import com.example.springbootone.entities.user;
 
 
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
